41a247c5c745bc34520c00f235f635c06693920c
Anton Gavrikov <gavrikovantonsergeevich@gmail.com>
2022-05-29 11:12:23 +0300

    test triggerTeamCity

---
+++ AssemblyInfocs.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+//using Stackoverflow domain as a made up example    
+
+// It is common, and mostly good, to use one GlobalAssemblyInfo.cs that is added 
+// as a link to many projects of the same product, details below
+// Change these attribute values in local assembly info to modify the information.
+[assembly: AssemblyProduct("Stackoverflow Q&A")]
+[assembly: AssemblyCompany("Test")]
+[assembly: AssemblyCopyright("Copyright © Stackoverflow 2016")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e4f2d33-aaab-48ea-a63d-1f0a8e3c935f")]
+[assembly: ComVisible(false)] //not going to expose ;)
+
+// Version information for an assembly consists of the following four values:
+// roughly translated from I reckon it is for SO, note that they most likely 
+// dynamically generate this file
+//      Major Version  - Year 6 being 2016
+//      Minor Version  - The month
+//      Day Number     - Day of month
+//      Revision       - Build number
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below: [assembly: AssemblyVersion("year.month.day.*")]
+[assembly: AssemblyVersion("2016.7.00.00")]
+[assembly: AssemblyFileVersion("2016.7.27.3839")]
